59b100196c3b944a2440e1c43f6529d318f50445
huiliu <dianalh1212@gmail.com>
2020-08-10 16:15:51 -0700

    Initial Commit

---
+++ AppStoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreApp
+//
+//  Created by hui liu on 8/10/20.
+//  Copyright © 2020 hui liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
